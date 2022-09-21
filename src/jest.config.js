076e89c4e690d73de726ffb6e49558ca3e51add8
selvagit <selvagcp2020@gmail.com>
2022-09-21 18:44:31 +0530

    Added integration test

---
+++ src/jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    testEnvironment: "node",
+    roots: ["<rootDir>/test"],
+    testMatch: ["**/*.test.js"],
+    reporters: [
+        "default",
+        [
+            "jest-junit",
+            {
+                outputDirectory: "test-reports",
+                outputName  : "test-report.xml",
+            }
+        ]
+    ]
+
+
+}
